eee29bf4ed1d5d8b502bd886bf47c1b922d4df78
Basil Minkov <proveyourselfmail@gmail.com>
2018-02-06 22:12:25 +0300

    added comments and started markdown

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Eye-Tracker-Data
+# Eye Tracker Data Processing 
+## HSE Methods and technologies of oculography homework assigments, 2018
+
+Here we have the code for eye tracker data processing.
+
+1. [Main](https://github.com/BasilMinkov/Behavioural-Experiment/tree/master/static/report) is a **Matlab** script from which all the functions are run.  
+2. [Detection of fixation](https://github.com/BasilMinkov/Eye-Tracker-Data/blob/master/fix_detect.m) is a **Matlab** 
+script with *Salvucci and Goldberg (2000)* algorythm replication.
--- main.m
+++ main.m
@@ -1,10 +1,10 @@
 clear all;
 
-fileID = fopen('S4a_20.dat');
-formatSpec = '%s';
-N = 4;
+% Assigment 1
 
-C_text = textscan(fileID,formatSpec,N,'Delimiter','	'); % header
+% read data from sample data 
+fileID = fopen('S4a_20.dat');
+C_text = textscan(fileID,'%s',4,'Delimiter','	'); % header
 C_data0 = textscan(fileID,'%d %d %f %f'); % columns as cells 
 
 % subset data for input
@@ -19,7 +19,7 @@ screen_pixel_width = 1440;
 screen_cm_width = 50;
 participant_distance = 85;
 v_threshold = 1; % as soon as I use deg/ms
-t_threshold = 5;
+t_threshold = 5; 
 
-
+% run custom function
 [ fix_sample_number,  fix_duration, fix_av_velocity] = fix_detect(time, XArr, YArr, velocity_to_degrees, minlength, screen_pixel_width, screen_cm_width, participant_distance, v_threshold, t_threshold)
